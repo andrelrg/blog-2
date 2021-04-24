58248116a38fb5a92709217077b0d9649060418e
André Gaspar <and_lrg@hotmail.com>
2021-04-24 17:31:22 -0300

    active items on navbar

--- src/molecules/NavBar.js
+++ src/molecules/NavBar.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Logo from '../atoms/Logo'
 import Link from '../atoms/NavLink'
 import styled from 'styled-components'
+import { useRouter } from 'next/router'
+
 
 const StyledHr = styled.hr`
     box-shadow: 2px 3px 5px black;
@@ -24,15 +26,25 @@ const NavHolder = styled.div`
 `;
 
 function NavBar() {
+    
+    const router = useRouter()
+    let links = {
+        home: router.pathname == "/",
+        posts: router.pathname == "/posts",
+        threads: router.pathname == "/threads",
+        about: router.pathname == "/about"
+
+    }
+
     return (
         <NavHolder>
             <StyledNav>
                 <Logo />
                 <LinksHolder>
-                   <Link href="/" active="true">Home</Link>
-                   <Link href="/posts">Posts</Link>
-                   <Link href="/threads">Threads</Link>
-                   <Link href="/about">About</Link>
+                   <Link href="/" active={links.home}>Home</Link>
+                   <Link href="/posts" active={links.posts}>Posts</Link>
+                   <Link href="/threads" active={links.threads}>Threads</Link>
+                   <Link href="/about" active={links.about}>About</Link>
                 </LinksHolder>
             </StyledNav>
             <StyledHr ></StyledHr>
